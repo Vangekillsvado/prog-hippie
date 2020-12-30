3d5012eaab6acbe8bc856211cc10d3053d26a34b
Vangekillsvado <aclarke500@gmail.com>
2020-12-30 11:31:46 -0500

    messedStuffUp

--- start.py
+++ start.py
@@ -1,14 +1,20 @@
-<<<<<<< HEAD
 import random
-n = random.randint(0,22)
-guess = input("Enter a number")
-pog = str(n)
-print (guess + ", " + pog)
-=======
 import stuff
+import hint 
+n = random.randint(0,22)
 
-ans = stuff.compare(12, 4)
+pog = str(n)
 
-print("starting " + str(ans))
-
->>>>>>> cf6d8a1a01396ef341dcdbfe7111a37f99acbb71
+windex = 0
+theNum = "The number is "
+guess = input("Enter a number")
+g = int(guess)
+print (guess + ", " + pog)
+ans = stuff.compare(g, n)
+while True:
+    if ans == 0:
+        print("Congrats")
+        break
+    else:
+        ##theHint = stuff.hint
+        print(stuff.hints(guess, windex))
